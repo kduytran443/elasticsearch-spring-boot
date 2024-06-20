315448962b95b59332557b7d3b69d358a79b0b58
kduytran443 <trankhanhduy18@gmail.com>
2024-06-21 04:27:42 +0700

    Make basic constant and dto

---
+++ src/main/java/com/kduytran/elasticsearch/constant/ResponseConstant.java
@@ -0,0 +1,12 @@
+package com.kduytran.elasticsearch.constant;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ResponseConstant {
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request processed successfully";
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Document created successfully";
+
+}

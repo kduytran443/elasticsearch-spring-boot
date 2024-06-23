4751f46ea5de04c4695d38c1a6dfb5d3a7432fb6
kduytran443 <trankhanhduy18@gmail.com>
2024-06-23 20:19:13 +0700

    Make SearchRequestDTO and SearchUtils to build SearchRequest and QueryBuilder

---
+++ src/main/java/com/kduytran/elasticsearch/search/SearchRequestDTO.java
@@ -0,0 +1,11 @@
+package com.kduytran.elasticsearch.search;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SearchRequestDTO {
+    private List<String> fields;
+    private String searchTerm;
+}

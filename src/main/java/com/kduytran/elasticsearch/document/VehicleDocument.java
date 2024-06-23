14921d14ec09fb166c27ad7ca42285786d37a60b
kduytran443 <trankhanhduy18@gmail.com>
2024-06-23 23:47:40 +0700

    Update attribute for Vehicle

--- src/main/java/com/kduytran/elasticsearch/document/VehicleDocument.java
+++ src/main/java/com/kduytran/elasticsearch/document/VehicleDocument.java
@@ -1,9 +1,15 @@
 package com.kduytran.elasticsearch.document;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
+
+import java.util.Date;
 
 @Data
 public class VehicleDocument {
     private String id;
     private String number;
+    private String name;
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    private Date created;
 }
